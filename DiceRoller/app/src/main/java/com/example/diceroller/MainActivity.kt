a811a2bbf160e7978b87bdba83960bb4accd52e2
migueldlcc <mdelacruzcabello@blc.edu>
2022-01-24 00:05:49 -0600

    First Commit

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,43 +3,41 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage1 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage1 = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        val countButton: Button = findViewById(R.id.countup_button)
-        countButton.setOnClickListener { countUp() }
-        val resetButton: Button = findViewById(R.id.reset_button)
 
-        resetButton.setOnClickListener {
-            resetButton.text = "0"
-        }
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clear()}
+
+
     }
 
     private fun rollDice() {
-        val randomInit = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInit.toString()
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(drawableResource)
+
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        }
-        else {
-            var resultInt = resultText.text.toString().toInt()
-            if (resultInt < 6) {
-                resultInt ++
-                resultText.text = resultInt.toString()
-            }
-        }
+    private fun clear() {
+        diceImage1.setImageResource(R.drawable.empty_dice)
     }
-
 }
